dd58446aeb94f64d6d08ad3f98accea99a9e074c
amit kumar <kumaramit19100@gmail.com>
2022-05-02 18:43:22 +0530

    Initial commit

---
+++ WindowsRegistry_07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace WindowsRegistry_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"Environment\");
+            var val = registryKey.GetValueNames();
+            foreach(var v in val)
+            {
+                Console.WriteLine(v);
+            }
+
+            var val2 = registryKey.GetValue("TEMP");
+            Console.WriteLine(val2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
