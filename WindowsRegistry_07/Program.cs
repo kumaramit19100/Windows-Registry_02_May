55874f4d334561fe9e997cc4e96ca6ef268b2dda
amit kumar <kumaramit19100@gmail.com>
2022-05-04 13:47:41 +0530

    update 2

--- WindowsRegistry_07/Program.cs
+++ WindowsRegistry_07/Program.cs
@@ -13,23 +13,38 @@ namespace WindowsRegistry_07
         {
             RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"Environment\");
             var val = registryKey.GetValueNames();
-            foreach(var v in val)
+            foreach (var v in val)
             {
-                Console.WriteLine("Value Name : "+v);
+                Console.WriteLine("Value Name : " + v);
             }
 
             var val2 = registryKey.GetValue("TEMP");
-            Console.WriteLine("Value is : "+val2.ToString());
+            Console.WriteLine("Value is : " + val2.ToString());
             Console.WriteLine("------------------------------------------------------------------------------------------");
-            
-            
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Console\");
+            var val3 = key.GetSubKeyNames();
+            foreach (var v in val3)
+            {
+                Console.WriteLine("Subkey Name : " + v);
+            }
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+
+            var val4 = key.GetValueNames();
+            foreach (var v in val4)
+            {
+                Console.WriteLine("Values Name : " + v);
+            }
+            Console.WriteLine("------------------------------------------------------------------------------------------");
 
 
 
 
 
 
-            
+
+
+
             Console.SetWindowSize(90, 33);  ///Set output windows size 
             //Console.Beep(1000, 10000);  ///a beep sound set on console output
             Console.ReadKey();
